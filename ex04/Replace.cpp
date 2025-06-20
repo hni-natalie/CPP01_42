ef2836c369a3877302afa8769aa66c83df424e1a
hni-natalie <natalieho061@gmail.com>
2025-06-21 04:03:54 +0800

    upload ex04

--- ex04/Replace.cpp
+++ ex04/Replace.cpp
@@ -14,16 +14,40 @@
 
 Replace::Replace(std::string filename, std::string s1, std::string s2): _s1(s1), _s2(s2) {
 	_infile.open(filename.c_str(), std::ios::in);
-	if(!_infile.is_open())
+	if(!_infile)
 	{
 		std::cerr << "Failed to open file." << std::endl;
 		std::exit(1);
 	}
 
 	_outfile.open((filename + ".replace").c_str(), std::ios::out | std::ios::trunc);
-	if (!_outfile.is_open())
+	if (!_outfile)
 	{
 		std::cerr << "Failed to create file!" << std::endl;
 		std::exit(1);
 	}
 } 
+
+Replace::~Replace() {
+	_infile.close();
+	_outfile.close();
+}
+
+void Replace::replace_all() {
+	size_t pos;
+	size_t found;
+	std::string line;
+
+	while (std::getline(_infile, line))
+	{
+		pos = 0;
+		// std::string::npos means not a position in the string
+		while ((found = line.find(_s1, pos)) != std::string::npos)
+		{
+			_outfile << line.substr(pos, found - pos);
+			_outfile << _s2;
+			pos = found + _s1.length(); 
+		}
+		_outfile << line.substr(pos) << "\n";
+	}
+}
--- ex04/main.cpp
+++ ex04/main.cpp
@@ -1,12 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   Replace.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hni-xuan <hni-xuan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/06/19 17:56:18 by hni-xuan          #+#    #+#             */
-/*   Updated: 2025/06/19 17:56:19 by hni-xuan         ###   ########.fr       */
+/*   Created: 2025/06/19 17:56:41 by hni-xuan          #+#    #+#             */
+/*   Updated: 2025/06/20 16:47:04 by hni-xuan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Replace.hpp"
+
+int main(int argc, char **argv) 
+{
+	if (argc != 4)
+	{
+		std::cerr << "Usage: ./replace <filename> <s1> <s2>" << std::endl;
+		return 1; 
+	}
+
+	Replace replacer(argv[1], argv[2], argv[3]);
+	replacer.replace_all();
+
+	return 0;
+}
