5804eec123147390035dce081f0aba78cba052a3
Ho Ni Xuan <hni-xuan@u90z05s01.42kl.edu.my>
2025-06-20 16:48:14 +0800

    update Makefile and ex04

--- ex04/Replace.cpp
+++ ex04/Replace.cpp
@@ -1,12 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Sed.cpp                                            :+:      :+:    :+:   */
+/*   Replace.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hni-xuan <hni-xuan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/19 17:56:41 by hni-xuan          #+#    #+#             */
-/*   Updated: 2025/06/19 17:56:42 by hni-xuan         ###   ########.fr       */
+/*   Updated: 2025/06/20 16:47:04 by hni-xuan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Replace.hpp"
+
+Replace::Replace(std::string filename, std::string s1, std::string s2): _s1(s1), _s2(s2) {
+	_infile.open(filename.c_str(), std::ios::in);
+	if(!_infile.is_open())
+	{
+		std::cerr << "Failed to open file." << std::endl;
+		std::exit(1);
+	}
+
+	_outfile.open((filename + ".replace").c_str(), std::ios::out | std::ios::trunc);
+	if (!_outfile.is_open())
+	{
+		std::cerr << "Failed to create file!" << std::endl;
+		std::exit(1);
+	}
+} 
--- ex04/Replace.hpp
+++ ex04/Replace.hpp
@@ -1,20 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Sed.hpp                                            :+:      :+:    :+:   */
+/*   Replace.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hni-xuan <hni-xuan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/19 17:57:01 by hni-xuan          #+#    #+#             */
-/*   Updated: 2025/06/19 18:10:48 by hni-xuan         ###   ########.fr       */
+/*   Updated: 2025/06/20 16:43:20 by hni-xuan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SED_HPP
-#define SED_HPP
+#ifndef REPLACE_HPP
+#define REPLACE_HPP
 
-class Sed {
-	
+#include <fstream>
+#include <iostream>
+#include <cstdlib>
+
+class Replace {
+	private:
+		std::ifstream _infile;
+		std::ofstream _outfile;
+		std::string _s1;
+		std::string _s2;
+		
+	public:
+		Replace(std::string filename, std::string s1, std::string s2);
+		~Replace();
+		void replace_all();
 };
 
 #endif
